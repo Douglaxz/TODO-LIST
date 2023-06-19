18a7e628e6dd1ccb652a4b00c6a7c28289d6474e
Douglas Amaral <douglaxz@hotmail.com>
2023-06-19 11:28:58 -0300

    transição

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,9 +1,23 @@
 <template>
   <v-main class="d-flex justify-center align-center">
-    <router-view />
+    <transition name="slide-x-transition" mode="out-in">
+      <router-view />
+    </transition>
   </v-main>
 </template>
 
 <script setup>
 //
 </script>
+
+<style>
+.slide-x-transition-enter-active,
+.slide-x-transition-leave-active {
+  transition: transform 0.3s ease-out;
+}
+
+.slide-x-transition-enter,
+.slide-x-transition-leave-to {
+  transform: translateX(100%);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,3 +5,5 @@
 <script setup>
 import Login from "@/components/Login.vue";
 </script>
+
+
