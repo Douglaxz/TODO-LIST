aa269a89c5306156a7f80a4b27caa38bef4cf2fc
Douglas Amaral <douglaxz@hotmail.com>
2023-06-19 10:38:05 -0300

    primeiro commit

---
+++ src/components/Login.vue
@@ -0,0 +1,129 @@
+<template>
+  <div
+    class="d-flex flex-column flex-md-row rounded-xl justify-center rounded-xl overflow-hidden"
+    :class="geralClassCss"
+    style="border-width: 2px; border-color: black; border-style: solid"
+  >
+    <div
+      class="d-flex flex-column align-center bg-black justify-space-evenly"
+      :class="iconeClassCss"
+    >
+      <h3>Bem-vindo ao TODO List</h3>
+      <p class="text-center pa-2">
+        Informe seu dados para login ou clique em Cadastro
+      </p>
+      <div
+        class="iconeCirculo bg-white d-flex align-center justify-center"
+        style="width: 170px; height: 170px; border-radius: 50%"
+      >
+        <span
+          class="mdi mdi-list-status text-black"
+          style="font-size: 90px"
+        ></span>
+      </div>
+      <RouterLink to="/Cadastro"
+        ><v-btn
+          prepend-icon="mdi-account-plus-outline"
+          variant="outlined"
+          color="white"
+        >
+          Cadastro
+        </v-btn></RouterLink
+      >
+    </div>
+    <div
+      class="d-flex flex-sm-column align-center justify-center justify-sm-space-evenly"
+      :class="formularioClassCss"
+    >
+      <v-sheet width="300" class="mx-auto">
+        <v-form v-model="formValid" fast-fail @submit.prevent>
+          <v-text-field
+            v-model="email"
+            append-icon="mdi-account-circle"
+            label="E-mail"
+            :rules="emailRules"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            label="Senha"
+            hint="Senha necessária"
+            @click:append="show1 = !show1"
+          ></v-text-field>
+
+          <v-btn
+            type="submit"
+            block
+            class="mt-2"
+            :disabled="!formValid"
+            @click="handleSubmit"
+            >Login</v-btn
+          >
+        </v-form>
+      </v-sheet>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    formValid: true,
+    email: "",
+    emailRules: [
+      function (value) {
+        const emailRegex =
+          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$/;
+
+        if (!emailRegex.test(value)) {
+          return "E-mail inválido";
+        }
+      },
+    ],
+    password: "",
+    rules: {
+      required: (value) => !!value || "Senha necessária.",
+    },
+  }),
+  methods: {
+    handleSubmit(event) {
+      event.preventDEfault();
+      if (!this.isFormValid) {
+        alert("Favor preencher todos os campos corretamente");
+        return;
+      }
+      console.log(this.isFormValid);
+    },
+  },
+  computed: {
+    geralClassCss() {
+      return {
+        "w-100": this.$vuetify.display.smAndDown,
+        "w-50": this.$vuetify.display.mdAndUp,
+        "h-100": this.$vuetify.display.smAndDown,
+        "h-75": this.$vuetify.display.mdAndUp,
+      };
+    },
+    formularioClassCss() {
+      return {
+        "h-50": this.$vuetify.display.smAndDown,
+        "h-100": this.$vuetify.display.mdAndUp,
+        "w-100": this.$vuetify.display.smAndDown,
+        "w-75": this.$vuetify.display.mdAndUp,
+      };
+    },
+    iconeClassCss() {
+      return {
+        "h-50": this.$vuetify.display.smAndDown,
+        "h-100": this.$vuetify.display.mdAndUp,
+        "w-100": this.$vuetify.display.smAndDown,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped></style>
